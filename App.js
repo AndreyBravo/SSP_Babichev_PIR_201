128b38a92b6ab90ade521e0b06acb2a4bd0ab922
Andrey Babichev <klopipa@gmail.com>
2024-02-21 17:39:13 +0300

    done lab 4

--- App.js
+++ App.js
@@ -1,55 +1,83 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  Text,
+  Button,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.first}>
-        <Text style={{ fontSize: 24 }}>5 книжных новинок</Text>
-      </View>
-      <View style={styles.second}>
-        <Text>Высокая чувствительность как талант</Text>
-      </View>
-      <View style={styles.third}>
-        <Text>
-          Способность тонко чувствительность иногда считается недостатком, но
-          психологи смело заявляют, что на самом деле очень крутой
-          талант.Способность тонко чувствительность иногда считается
-          недостатком, но психологи смело заявляют, что на самом деле очень
-          крутой талант.Способность тонко чувствительность иногда считается
-          недостатком, но психологи смело заявляют, что на самом деле очень
-          крутой талант.
-        </Text>
-      </View>
-      <Button
-        title="Читать далее"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-    </View>
-  );
-}
+const { width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-  },
-  first: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
-  second: {
+  slideContainer: {
     flex: 2,
-    backgroundColor: "#C0C0C0",
-    alignItems: "center",
+    width,
     justifyContent: "center",
+    alignItems: "center",
   },
-  third: {
-    flex: 3,
-    backgroundColor: "#808080",
-    padding: 20,
+  title: { flex: 1, fontSize: 30, paddingTop:100 },
+  content: { flex: 3 },
+  image: {
+    width: 250,
+    height: 250,
+    resizeMode: "cover",
+  },
+  more: {
+    
   },
 });
+
+const App = () => {
+  const scrollViewRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const images = [
+    "https://brightmagazine.ru/wp-content/uploads/2024/02/adrien-king-XzBluXxBoyQ-unsplash-600x400.jpg",
+    "https://brightmagazine.ru/wp-content/uploads/2024/02/parabol-the-agile-meeting-toolbox-BDfQnva_6mU-unsplash-768x512.jpg",
+    "https://brightmagazine.ru/wp-content/uploads/2024/02/greg-rosenke-OqP4uMDQ1e0-unsplash-768x512.jpg",
+  ];
+
+  const handleScroll = (event) => {
+    const slide = Math.round(event.nativeEvent.contentOffset.x / width);
+    setCurrentSlide(slide);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>My travel</Text>
+      <ScrollView
+        ref={scrollViewRef}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={handleScroll}
+        scrollEventThrottle={200}
+        decelerationRate="fast"
+      >
+        {images.map((image, index) => (
+          <View key={index} style={styles.slideContainer}>
+            <Image source={{ uri: image }} style={styles.image} />
+          </View>
+        ))}
+      </ScrollView>
+      <Text style={styles.content}>
+        Существует достаточно много вредных привычек, которые мешают нам
+        становится лучше, тормозят наш рост и развитие. И это не только те самые
+        вредные привычки, о которых часто говорят диетологи и врачи. Часто это
+        именно психологические проблемы, от которых, если поработать над собой,
+        можно избавиться самостоятельно
+      </Text>
+      <Button onPress={()=>console.log("Press button")} style={styles.more} title="Learn More" color="#841584" />
+    </View>
+  );
+};
+
+export default App;
