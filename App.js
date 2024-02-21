cbd06b522e3182bf30adb904c368ca8ac057f096
Andrey Babichev <klopipa@gmail.com>
2024-02-21 16:34:04 +0300

    done lab 3

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.first}>
+        <Text style={{ fontSize: 24 }}>5 книжных новинок</Text>
+      </View>
+      <View style={styles.second}>
+        <Text>Высокая чувствительность как талант</Text>
+      </View>
+      <View style={styles.third}>
+        <Text>
+          Способность тонко чувствительность иногда считается недостатком, но
+          психологи смело заявляют, что на самом деле очень крутой
+          талант.Способность тонко чувствительность иногда считается
+          недостатком, но психологи смело заявляют, что на самом деле очень
+          крутой талант.Способность тонко чувствительность иногда считается
+          недостатком, но психологи смело заявляют, что на самом деле очень
+          крутой талант.
+        </Text>
+      </View>
+      <Button
+        title="Читать далее"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
@@ -13,8 +33,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  first: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  second: {
+    flex: 2,
+    backgroundColor: "#C0C0C0",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  third: {
+    flex: 3,
+    backgroundColor: "#808080",
+    padding: 20,
   },
 });
